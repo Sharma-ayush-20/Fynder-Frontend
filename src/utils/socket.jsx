808b7a4964717e7c5541e274b950845e30f08dca
Ayush Sharma <149467415+Sharma-ayush-20@users.noreply.github.com>
2026-09-15 23:35:47 +0530

    Simplify socket connection creation
    Refactor socket connection logic to always use baseUrl with credentials.

--- src/utils/socket.jsx
+++ src/utils/socket.jsx
@@ -2,12 +2,14 @@ import { io } from "socket.io-client";
 import { baseUrl } from "./constants";
 
 export const createSocketConnection = () => {
-    if(location.hostname === "localhost"){
-        //localhost
-        return io(`${baseUrl}`)
-    }else{
-        //production
-        return io("/", {path: "/api/socket.io"})
-    }
-    
+    // if(location.hostname === "localhost"){
+    //     //localhost
+    //     return io(`${baseUrl}`)
+    // }else{
+    //     //production
+    //     return io("/", {path: "/api/socket.io"})
+    // }
+    return io(baseUrl, {
+    withCredentials: true
+});
 }
